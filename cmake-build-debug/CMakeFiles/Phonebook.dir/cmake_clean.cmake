d951c2dae7f73e0790d632b0a805ebf42fea39aa
Omar Alaa <omar.skillful@gmail.com>
2019-12-22 01:34:03 +0200

    Adding yosra changes and new cmake files

---
+++ cmake-build-debug/CMakeFiles/Phonebook.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Phonebook.dir/io.c.o"
+  "CMakeFiles/Phonebook.dir/main.c.o"
+  "CMakeFiles/Phonebook.dir/models.c.o"
+  "CMakeFiles/Phonebook.dir/useroptions.c.o"
+  "CMakeFiles/Phonebook.dir/validation.c.o"
+  "Phonebook"
+  "Phonebook.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Phonebook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
